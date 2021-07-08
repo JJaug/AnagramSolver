9ec7ac72493ac144dccf826852f15bd1eefa7177
JJaugelis <jonas.jaugas@gmail.com>
2021-07-08 16:04:32 +0300

    Parameter for test method and SetUp (WordRepository)

--- AnagramSolver.Tests/WordRepositoryTests.cs
+++ AnagramSolver.Tests/WordRepositoryTests.cs
@@ -7,23 +7,26 @@ namespace AnagramSolver.Tests
     [TestFixture]
     public class WordRepositoryTests
     {
-        [SetUp]
+        private WordRepository _wordRepository;
+
+        [SetUp] public void Setup()
+        {
+            _wordRepository = new WordRepository();
+        }
 
 
         [Test]
-        public void Should_ReturnListOfWords_When_GivenFileOfWords()
+        [TestCase("C:\\Users\\jonas.jaugelis\\source\\repos\\AnagramSolver\\zodynas.txt")]
+        public void Should_ReturnListOfWords_When_GivenFileOfWords(string filePath)
         {
-            var filePath = "C:\\Users\\jonas.jaugelis\\source\\repos\\AnagramSolver\\zodynas.txt";
-            var wordRepository = new WordRepository();
-            var allWords = wordRepository.GetAllWords(@filePath, 4);
+            var allWords = _wordRepository.GetAllWords(@filePath, 4);
             Assert.That(allWords, !Is.Null);
         }
         [Test]
-        public void Should_ReturnNull_When_GivenWrongFilePath()
+        [TestCase("C:\\Users\\jonas.jaugelis\\source\\repos\\AnagramSolver\\zodynas2.txt")]
+        public void Should_ReturnNull_When_GivenWrongFilePath(string filePath)
         {
-            var filePath = "C:\\Users\\jonas.jaugelis\\source\\repos\\AnagramSolver\\zodynas2.txt";
-            var wordRepository = new WordRepository();
-            var allWords = wordRepository.GetAllWords(@filePath, 4);
+            var allWords = _wordRepository.GetAllWords(@filePath, 4);
             Assert.IsNull(allWords);
         }
     }
