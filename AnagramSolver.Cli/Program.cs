d80450c3ff32b13919d6d32887722c18b9f55aa4
JJaugelis <jonas.jaugas@gmail.com>
2021-07-19 09:30:21 +0300

    Giving DB words from file thru console app

--- AnagramSolver.Cli/Program.cs
+++ AnagramSolver.Cli/Program.cs
@@ -1,10 +1,13 @@
 ﻿using AnagramSolver.BusinessLogic.Classes;
 using AnagramSolver.Contracts.Interfaces;
+using AnagramSolver.Models.Models;
 using AnagramSolver.WebApp.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -17,6 +20,7 @@ namespace AnagramSolver.Cli
         static async Task Main(string[] args)
         {
             ConfigureAppSettings(out int minLength, out int maxAnagrams, out string filePath, out string anagramApi);
+            Insert(filePath, minLength);
             IWordRepository wordRepository = new WordDBRepository();
             var allWords = wordRepository.GetAllWords();
             var result = new BusinessLogic.Classes.AnagramSolver(allWords);
@@ -66,6 +70,44 @@ namespace AnagramSolver.Cli
             maxAnagrams = Int32.Parse(config["MaxNumberOfAnagrams"]);
             anagramApi = config["AnagramApi"];
             filePath = config["FilePath"];
+        }
+        private static void Insert(string filePath, int minLength)
+        {
+            int id = 1;
+            HashSet<string> allLines;
+            allLines = new HashSet<string>(File.ReadLines(filePath));
+            var newList = new HashSet<string>();
+            string connectionString = @"Data Source=LT-LIT-SC-0597\MSSQLSERVER01;Initial Catalog=VocabularyDB;Integrated Security=True";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    foreach (string line in allLines)
+                    {
+                        string[] wordsInLine = line.Split("\t").ToArray();
+                        if (wordsInLine[2].Length >= minLength)
+                        {
+                            var word = wordsInLine[2];
+                            newList.Add(word);
+                        }
+                    }
+                    foreach (string word in newList)
+                    {
+                        SqlCommand cmd = new SqlCommand("INSERT INTO Words (ID, Word) VALUES (@id, @word)", conn);
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.Parameters.AddWithValue("@word", word);
+                        cmd.ExecuteNonQuery();
+                        id++;
+                    }
+                    conn.Close();
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
         }
     }
 }
